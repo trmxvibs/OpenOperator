f1d9cf39f78a2071920f0ffacd021106e6d4be04
Lokesh Kumar <109752684+trmxvibs@users.noreply.github.com>
2026-06-16 23:34:33 +0530

    Add ActionController abstract class for actions

---
+++ src/openoperator/action/base.py
@@ -0,0 +1,17 @@
+from abc import ABC
+from abc import abstractmethod
+
+
+class ActionController(ABC):
+
+    @abstractmethod
+    def move_mouse(self, x: int, y: int) -> bool:
+        pass
+
+    @abstractmethod
+    def click(self, button: str = "left") -> bool:
+        pass
+
+    @abstractmethod
+    def type_text(self, text: str) -> bool:
+        pass
